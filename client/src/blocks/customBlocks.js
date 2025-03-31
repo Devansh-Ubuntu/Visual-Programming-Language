1e557b03b7352d7d16ea19dc820489ad1cd9a4e2
arpit25112004 <goyalarpit379@gmail.com>
2025-03-31 12:24:13 +0530

    Initial commit

--- client/src/blocks/customBlocks.js
+++ client/src/blocks/customBlocks.js
@@ -21,3 +21,86 @@ Blockly.Blocks['print_hello'] = {
     this.setHelpUrl('');
   }
 };
+// Stack Implementation
+Blockly.Blocks['stack_push'] = {
+  init: function () {
+    this.appendValueInput("VALUE").setCheck(null).appendField("Push to Stack");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip("Push an element onto the stack.");
+  }
+};
+
+Blockly.Blocks['stack_pop'] = {
+  init: function () {
+    this.appendDummyInput().appendField("Pop from Stack");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip("Remove the top element from the stack.");
+  }
+};
+
+// Queue Implementation
+Blockly.Blocks['queue_enqueue'] = {
+  init: function () {
+    this.appendValueInput("VALUE").setCheck(null).appendField("Enqueue to Queue");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("Add an element to the back of the queue.");
+  }
+};
+
+Blockly.Blocks['queue_dequeue'] = {
+  init: function () {
+    this.appendDummyInput().appendField("Dequeue from Queue");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("Remove the front element from the queue.");
+  }
+};
+
+// Linked List Implementation
+Blockly.Blocks['linkedlist_insert'] = {
+  init: function () {
+    this.appendValueInput("VALUE").setCheck(null).appendField("Insert in Linked List");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(220);
+    this.setTooltip("Insert an element into the linked list.");
+  }
+};
+
+Blockly.Blocks['linkedlist_delete'] = {
+  init: function () {
+    this.appendValueInput("VALUE").setCheck(null).appendField("Delete from Linked List");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(220);
+    this.setTooltip("Delete an element from the linked list.");
+  }
+};
+
+// Doubly Linked List Implementation
+Blockly.Blocks['doublylist_insert'] = {
+  init: function () {
+    this.appendValueInput("VALUE").setCheck(null).appendField("Insert in Doubly Linked List");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip("Insert an element into the doubly linked list.");
+  }
+};
+
+Blockly.Blocks['doublylist_delete'] = {
+  init: function () {
+    this.appendValueInput("VALUE").setCheck(null).appendField("Delete from Doubly Linked List");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip("Delete an element from the doubly linked list.");
+  }
+};
